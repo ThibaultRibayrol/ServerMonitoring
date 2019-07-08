bd36427aaabcd48f48ed1832dc39cef838dc8588
unknown <thibault.ribayrol@gmail.com>
2019-07-08 11:20:18 +0200

    merge

--- watching_launcher.sh
+++ watching_launcher.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 ###
-# Une solution est d'utiliser cette command `pidof /usr/local/bin/node`, puis parser les 4 pids et écouter sur les 4.
+# Run this script to watch the four server process.
+# If one is down, a mail is send to the email bellow.
 ###
 
 sudo forever restart f0Py
 
 pidArray=`pidof /usr/local/bin/node`
 
-echo $pidArray
+#echo $pidArray
 
 A="$(echo $pidArray | cut -d' ' -f1)"
 B="$(echo $pidArray | cut -d' ' -f2)"
 C="$(echo $pidArray | cut -d' ' -f3)"
 D="$(echo $pidArray | cut -d' ' -f4)"
-
-echo $A
-echo $B
-echo $C
-echo $D
 
 nohup `./process_watcher.py --pid $A --pid $B --pid $C --pid $D --to thibault.ribayrol@gmail.com` &
